bb307d70a3b0339176c48fee66a508b5d02a682f
Willy Ovalle <willyovalle16@gmail.com>
2019-05-09 23:11:18 +0200

    feat: finally working!

--- packages/katas/functional-sql/kata.js
+++ packages/katas/functional-sql/kata.js
@@ -1,20 +1,39 @@
 class Query {
   constructor() {
-    this.collection = [];
-    this.mapFn = null;
+    this.collection = null;
+    this.selectFn = null;
     this.whereFns = [];
     this.groupByFns = null;
     this.orderByFn = null;
-    this.havingFn = null;
+    this.havingFns = [];
   }
 
   select(fn) {
-    this.mapFn = fn;
+    if (this.selectFn) {
+      throw new Error('Duplicate SELECT');
+    }
+    this.selectFn = fn ? fn : e => e;
     return this;
   }
 
-  from(col) {
-    this.collection = col;
+  from(...cols) {
+    if (this.collection) {
+      throw new Error('Duplicate FROM');
+    }
+
+    if (cols.length > 1) {
+      const mergedCollections = [];
+      for (let i = 0; i < cols.length - 1; i++) {
+        for (let j = 0; j < cols.length; j++) {
+          for (let k = 0; k < cols[i].length; k++) {
+            mergedCollections.push([cols[i][j], cols[i + 1][k]]);
+          }
+        }
+      }
+      this.collection = mergedCollections || [];
+    } else {
+      this.collection = cols[0] || [];
+    }
     return this;
   }
 
@@ -24,30 +43,38 @@ class Query {
   }
 
   groupBy(...fns) {
+    if (this.groupByFns) {
+      throw new Error('Duplicate GROUPBY');
+    }
+
     this.groupByFns = fns;
     return this;
   }
 
   orderBy(fn) {
+    if (this.orderByFn) {
+      throw new Error('Duplicate ORDERBY');
+    }
+
     this.orderByFn = fn;
     return this;
   }
 
-  having(fn) {
-    this.havingFn = fn;
+  having(...fns) {
+    this.havingFns.push(fns);
     return this;
   }
 
   execute() {
-    let data = this.collection;
+    let data = this.collection || [];
 
     if (this.whereFns.length) {
-      data = this.whereFns.reduce((_, fns) => {
-        return fns.reduce((acc2, fn) => {
-          acc2 = acc2.concat(data.filter(fn).reverse());
-          return acc2;
+      data = this.whereFns.reduce((andAcc, fns) => {
+        return fns.reduce((acc, fn) => {
+          acc = acc.concat(andAcc.filter(fn));
+          return acc;
         }, []);
-      }, []);
+      }, data);
     }
 
     if (this.groupByFns) {
@@ -58,7 +85,7 @@ class Query {
 
         const obj = arr.reduce((acc, cur) => {
           const k = fns[0](cur);
-          acc.set(k, acc.get(k) ? acc.get(k).concat(cur) : [cur]);
+          acc.set(k, acc.get(k) ? acc.get(k).concat([cur]) : [cur]);
           return acc;
         }, new Map());
 
@@ -70,18 +97,23 @@ class Query {
       data = groupBy(data, this.groupByFns);
     }
 
+    if (this.havingFns.length) {
+      data = this.havingFns.reduce((_, fns) => {
+        return fns.reduce((acc, fn) => {
+          return acc.concat(data.filter(fn));
+        }, []);
+      }, []);
+    }
+
+    if (this.selectFn) {
+      data = data.map(this.selectFn);
+    }
+
     if (this.orderByFn) {
       data = data.sort(this.orderByFn);
     }
 
-    if (this.mapFn) {
-      data = data.map(this.mapFn);
-    }
-
-    if (this.havingFn) {
-      data = data.filter(this.havingFn);
-    }
-
+    // console.log(JSON.stringify(data, null, 2));
     return data;
   }
 }
--- packages/katas/mocha.js
+++ packages/katas/mocha.js
@@ -2,4 +2,5 @@ const chai = require('chai');
 
 global.Test = {};
 global.Test.assertSimilar = chai.assert.deepEqual;
+global.Test.assertEquals = chai.assert.equal;
 global.Test.expect = chai.expect;
